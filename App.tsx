edc78c07e6c37f84e7b3eb0d09c198326fa88270
cluffa <cluffa@users.noreply.github.com>
2023-03-05 08:00:33 -0500

    update

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
